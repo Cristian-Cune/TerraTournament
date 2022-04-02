ab470d0dcc14f117fd3b3684eecf7cf4b88ece7e
Cristian Cune <56230546+Cristian-Cune@users.noreply.github.com>
2022-04-02 12:59:15 +0300

    Add base backend project

---
+++ TerraTournament/src/main/java/com/bestem/TerraTournament/TerraTournamentApplication.java
@@ -0,0 +1,13 @@
+package com.bestem.TerraTournament;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TerraTournamentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TerraTournamentApplication.class, args);
+	}
+
+}
